efb8fda3103d7a94b5a60c008cc78a0490ff7e2c
Arkko002 <radioaktywna.kapusta.z.czarnobyla@gmail.com>
2018-10-19 16:50:12 +0200

    Add project files.

---
+++ ThisIsAChat/Startup.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ThisIsAChat.Startup))]
+namespace ThisIsAChat
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+            app.MapSignalR();
+        }
+    }
+}
